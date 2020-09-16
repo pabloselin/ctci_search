54c9444a19e317e7d35992ee59fca16d88b398e1
Pablo Selín Carrasco Armijo <pablo@apie.cl>
2020-09-16 20:07:51 -0300

    docterms

--- src/DocItem.js
+++ src/DocItem.js
@@ -4,7 +4,6 @@ import latinize from "latinize";
 
 import DocDate from "./partials/DocDate.js";
 
-
 class DocItem extends Component {
 	constructor(props) {
 		super(props);
@@ -35,12 +34,32 @@ class DocItem extends Component {
 				<div className="docText">
 					<h1>
 						<a href={this.props.link}>
-						<Highlighter sanitize={latinize} searchWords={[this.props.searchQuery]} textToHighlight={this.props.title} />
+							<Highlighter
+								sanitize={latinize}
+								searchWords={[this.props.searchQuery]}
+								textToHighlight={this.props.title}
+							/>
 						</a>
-
 					</h1>
 					<DocDate date={this.props.date} />
-					<Highlighter sanitize={latinize} className="content" searchWords={[this.props.searchQuery]} textToHighlight={this.props.excerpt} />
+					<Highlighter
+						sanitize={latinize}
+						className="content"
+						searchWords={[this.props.searchQuery]}
+						textToHighlight={this.props.excerpt}
+					/>
+
+					<div className="docTerms">
+					{this.props.terms.docarea.length > 0 &&
+						this.props.terms.docarea.map((area) => (
+							<span>{area.name}</span>
+						))}
+
+						{this.props.terms.docpilar.length > 0 &&
+						this.props.terms.docpilar.map((area) => (
+							<span>{area.name}</span>
+						))}
+					</div>
 				</div>
 			</article>
 		);
